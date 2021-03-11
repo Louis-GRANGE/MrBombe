a5efc1f33abbf7f7cc601668cbbfe58a31d948c8
Louis-GRANGE <l.grange43@gmail.com>
2021-03-11 14:53:59 +0100

    [UPDATE] Add C++ Class
    To build the game with the plugin

---
+++ Source/MrBombe/MrBombe.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MrBombe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MrBombe, "MrBombe" );
